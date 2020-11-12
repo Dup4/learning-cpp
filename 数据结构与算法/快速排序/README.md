e5c16c5c150dae642f6a5fd0d5afda9137fbb7c8
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-12 12:47:15 +0800

    Add 快速排序

---
+++ 数据结构与算法/快速排序/README.md
@@ -0,0 +1,3 @@
+# 快速排序
+
+[提交地址](https://www.nowcoder.com/practice/2baf799ea0594abd974d37139de27896?tpId=117&&tqId=36039&rp=1&ru=/ta/job-code-high&qru=/ta/job-code-high/question-ranking)
---
+++ 数据结构与算法/快速排序/quick_sort.h
@@ -0,0 +1,31 @@
+class Solution {
+    int Partition(vector <int> &A, int l, int r) {
+        int pivot = A[l];
+        while (l < r) {
+            while (l < r && A[r] >= pivot) --r;
+            A[l] = A[r];
+            while (l < r && A[l] <= pivot) ++l;
+            A[r] = A[l];
+        }
+        A[l] = pivot;
+        return l;
+    }
+
+    void QuickSort(vector <int> &vec, int l, int r) {
+        if (l >= r) return;
+        int pos = Partition(vec, l, r);
+        QuickSort(vec, l, pos - 1);
+        QuickSort(vec, pos + 1, r);
+    }
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     * 将给定数组排序
+     * @param arr int整型vector 待排序的数组
+     * @return int整型vector
+     */
+    vector<int> MySort(vector<int>& arr) {
+        QuickSort(arr, 0, arr.size() - 1);
+        return arr;
+    }
+};
