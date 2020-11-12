5d9abeaea3236b961f3c94ceb44de378d13a8249
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-12 14:45:10 +0800

    Add 找到搜索二叉树中两个错误的节点

---
+++ 数据结构与算法/找到搜索二叉树中两个错误的节点/main.cpp
@@ -0,0 +1,38 @@
+/**
+ * struct TreeNode {
+ *	int val;
+ *	struct TreeNode *left;
+ *	struct TreeNode *right;
+ * };
+ */
+
+class Solution {
+public:
+    /**
+     * 
+     * @param root TreeNode类 the root
+     * @return int整型vector
+     */
+    TreeNode* pre;
+    TreeNode* fi;
+    TreeNode* se;
+    void dfs(TreeNode* rt) {
+        if (!rt) return;
+        dfs(rt->left);
+        if (pre && pre->val >= rt->val) {
+            if (!fi) fi = pre;
+            se = rt;
+        }
+        pre = rt;
+        dfs(rt->right);
+    }
+    
+    vector<int> findError(TreeNode* root) {
+        // write code here
+        pre = nullptr;
+        fi = nullptr;
+        se = nullptr;
+        dfs(root);
+        return {se->val, fi->val};
+    }
+};
