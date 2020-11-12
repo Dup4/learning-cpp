90c8f62cdb65cb5a8a617f441da2c932f75ba1ce
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-12 23:14:14 +0800

    Add 旋转数组

---
+++ 数据结构与算法/旋转数组/README.md
@@ -0,0 +1,3 @@
+# 旋转数组
+
+[提交链接](https://www.nowcoder.com/practice/e19927a8fd5d477794dac67096862042?tpId=117&&tqId=35035&rp=1&ru=/ta/job-code-high&qru=/ta/job-code-high/question-ranking)
---
+++ 数据结构与算法/旋转数组/main.h
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    /**
+     * 旋转数组
+     * @param n int整型 数组长度
+     * @param m int整型 右移距离
+     * @param a int整型vector 给定数组
+     * @return int整型vector
+     * 
+     */
+    vector<int> solve(int n, int m, vector<int>& a) {
+        m = m % n;
+        if (!m) return a;
+        reverse(a.begin() + n - m, a.end());
+        reverse(a.begin(), a.begin() + n - m);
+        reverse(a.begin(), a.end());
+        return a;
+    }
+};
