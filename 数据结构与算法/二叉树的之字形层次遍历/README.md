a7edfeb72e35a7fb090daf9113e67c81e74a91a5
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-12 15:53:52 +0800

    Add 二叉树的之字形层次遍历

---
+++ 数据结构与算法/二叉树的之字形层次遍历/README.md
@@ -0,0 +1,3 @@
+# 二叉树的之字形层次遍历
+
+[提交地址](https://www.nowcoder.com/practice/47e1687126fa461e8a3aff8632aa5559?tpId=117&&tqId=34935&rp=1&ru=/ta/job-code-high&qru=/ta/job-code-high/question-ranking)
---
+++ 数据结构与算法/二叉树的之字形层次遍历/main.h
@@ -0,0 +1,37 @@
+/**
+ * struct TreeNode {
+ *	int val;
+ *	struct TreeNode *left;
+ *	struct TreeNode *right;
+ * };
+ */
+
+class Solution {
+public:
+    /**
+     * 
+     * @param root TreeNode类 
+     * @return int整型vector<vector<>>
+     */
+    vector<vector<int> > zigzagLevelOrder(TreeNode* root) {
+        if (!root) return {};
+        queue <TreeNode*> que;
+        que.push(root);
+        vector <vector<int>> res;
+        int f = 0;
+        while (!que.empty()) {
+            int sze = static_cast<int>(que.size());
+            vector <int> vec;
+            for (int _ = 0; _ < sze; ++_) {
+                TreeNode* rt = que.front(); que.pop();
+                vec.push_back(rt->val);
+                if (rt->left != nullptr) que.push(rt->left);
+                if (rt->right != nullptr) que.push(rt->right);
+            }
+            if (f) reverse(vec.begin(), vec.end());
+            res.push_back(vec);
+            f ^= 1;
+        }
+        return res;
+    }
+};
