aa9ea5fa9e9df82775cdc24f72c36a5e6e623c6d
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-13 09:09:55 +0800

    Add 设计getMin功能的栈

---
+++ 数据结构与算法/设计getMin功能的栈/README.md
@@ -0,0 +1,3 @@
+# 设计getMin功能的栈
+
+[提交链接](https://www.nowcoder.com/practice/c623426af02d4c189f92f2a99647bd34?tpId=117&&tqId=35012&rp=1&ru=/ta/job-code-high&qru=/ta/job-code-high/question-ranking)
---
+++ 数据结构与算法/设计getMin功能的栈/Solution.h
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    /**
+     * return a array which include all ans for op3
+     * @param op int整型vector<vector<>> operator
+     * @return int整型vector
+     */
+    vector<int> getMinStack(vector<vector<int> >& op) {
+        vector <int> res;
+        stack <int> sta[2];
+        for (auto &it : op) {
+            if (it[0] == 1) {
+                sta[0].push(it[1]);
+                int Min = it[1];
+                if (!sta[1].empty()) {
+                    Min = min(Min, sta[1].top());
+                }
+                sta[1].push(Min);
+            } else if (it[0] == 2) {
+                sta[0].pop();
+                sta[1].pop();
+            } else {
+                res.push_back(sta[1].top());
+            }
+        }
+        return res;
+    }
+};
