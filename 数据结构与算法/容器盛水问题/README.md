f556a7995c5ab83eb17cd3b74911a52011896f8d
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-16 14:43:11 +0800

    Add 容器盛水问题

---
+++ 数据结构与算法/容器盛水问题/README.md
@@ -0,0 +1,3 @@
+# 容器盛水问题
+
+[提交链接](https://www.nowcoder.com/practice/31c1aed01b394f0b8b7734de0324e00f?tpId=117&&tqId=35269&rp=1&ru=/ta/job-code-high&qru=/ta/job-code-high/question-ranking)
---
+++ 数据结构与算法/容器盛水问题/Solution.h
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    /**
+     * max water
+     * @param arr int整型vector the array
+     * @return long长整型
+     */
+    long long maxWater(vector<int>& arr) {
+        int n = static_cast<int>(arr.size());
+        if (n < 3) return 0;
+        int l = 0, r = n - 1;
+        int wl = arr[0], wr = arr[n - 1];
+        long long sum = 0;
+        while (l <= r) {
+            if (arr[l] < arr[r]) {
+                if (arr[l] < wl) sum += wl - arr[l];
+                else wl = arr[l];
+                ++l;
+            } else {
+                if (arr[r] < wr) sum += wr - arr[r];
+                else wr = arr[r];
+                --r;
+            }
+        }
+        return sum;
+    }
+};
