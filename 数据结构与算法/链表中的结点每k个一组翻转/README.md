e4ba623d7b0e902191de24d0238951e974d411a1
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-16 15:24:54 +0800

    Add 链表中的结点每k个一组翻转

---
+++ 数据结构与算法/链表中的结点每k个一组翻转/README.md
@@ -0,0 +1,3 @@
+# 链表中的结点每k个一组翻转
+
+[提交链接](https://www.nowcoder.com/practice/b49c3dc907814e9bbfa8437c251b028e?tpId=117&&tqId=34971&rp=1&ru=/ta/job-code-high&qru=/ta/job-code-high/question-ranking)
---
+++ 数据结构与算法/链表中的结点每k个一组翻转/Solution.h
@@ -0,0 +1,43 @@
+/**
+ * struct ListNode {
+ *	int val;
+ *	struct ListNode *next;
+ * };
+ */
+
+class Solution {
+    int getLength(ListNode *head) {
+        int length = 0;
+        while (head != nullptr) {
+            length++;
+            head = head->next;
+        }
+        return length;
+    }
+    
+public:
+    /**
+     * 
+     * @param head ListNode类 
+     * @param k int整型 
+     * @return ListNode类
+     */
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (head == nullptr || head->next == nullptr || k == 1) return head;
+        ListNode *res = new ListNode(0);
+        res->next = head;
+        ListNode *pre = res, *cur = head, *tmp = nullptr;
+        int length = getLength(head);
+        for (int i = 0; i < length / k; ++i) {
+            for (int j = 1; j < k; ++j) {
+                tmp = cur->next;
+                cur->next = tmp->next;
+                tmp->next = pre->next;
+                pre->next = tmp;
+            }
+            pre = cur;
+            cur = cur->next;
+        }
+        return res->next;
+    }
+};
