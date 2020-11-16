d9daa86f28c10f8e8dc2725f9d165d88ea5bde0f
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-16 15:13:43 +0800

    Add 通配符匹配

---
+++ 数据结构与算法/通配符匹配/README.md
@@ -0,0 +1,3 @@
+# 通配符匹配
+
+[提交链接](https://www.nowcoder.com/practice/e96f1a44d4e44d9ab6289ee080099322?tpId=117&&tqId=34965&rp=1&ru=/ta/job-code-high&qru=/ta/job-code-high/question-ranking)
---
+++ 数据结构与算法/通配符匹配/Solution.h
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        int sLen = strlen(s), pLen = strlen(p);
+        vector <vector<bool>> f(sLen + 1, vector<bool>(pLen + 1, true));
+        for (int i = 1; i <= sLen; ++i) f[i][0] = false;
+        for (int i = 0; i < pLen; ++i) {
+            if (p[i] != '*') {
+                for (int j = i + 1; j <= pLen; ++j) f[0][j] = false;
+                break;
+            }
+        }
+        for (int i = 1; i <= sLen; ++i) {
+            for (int j = 1; j <= pLen; ++j) {
+                f[i][j] = false;
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') f[i][j] = f[i - 1][j - 1];
+                else if (p[j - 1] == '*') {
+                    f[i][j] = f[i - 1][j - 1] || f[i][j - 1] || f[i - 1][j];
+                }
+            }
+        }
+        return f[sLen][pLen];
+    }
+};
