d5c6884e0678e854c2883c4b1c5ae61faa9cc5b1
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-16 14:34:59 +0800

    Add 判断一个链表是否为回文结构

---
+++ 数据结构与算法/判断一个链表是否为回文结构/README.md
@@ -0,0 +1,3 @@
+# 判断一个链表是否为回文结构
+
+[提交链接](https://www.nowcoder.com/practice/3fed228444e740c8be66232ce8b87c2f?tpId=117&&tqId=35018&rp=1&ru=/ta/job-code-high&qru=/ta/job-code-high/question-ranking)
---
+++ 数据结构与算法/判断一个链表是否为回文结构/Solution.h
@@ -0,0 +1,42 @@
+/**
+ * struct ListNode {
+ *	int val;
+ *	struct ListNode *next;
+ * };
+ */
+
+class Solution {
+    bool isSame(ListNode *h1, ListNode *h2) {
+        while (h1 != nullptr && h2 != nullptr) {
+            if (h1->val != h2->val) return false;
+            h1 = h1->next;
+            h2 = h2->next;
+        }
+        return true;
+    }
+    ListNode* reverse(ListNode *head, ListNode *tail) {
+        ListNode *pre = nullptr, *cur = head;
+        while (cur != tail) {
+            ListNode* tmp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+public:
+    /**
+     * 
+     * @param head ListNode类 the head
+     * @return bool布尔型
+     */
+    bool isPail(ListNode* head) {
+        ListNode *pre = head, *cur = head;
+        while (cur != nullptr && cur->next != nullptr) {
+            cur = cur->next->next;
+            pre = pre->next;
+        }
+        ListNode *newHead = reverse(pre, cur);
+        return isSame(head, newHead);
+    }
+};
