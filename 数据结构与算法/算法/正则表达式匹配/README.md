eb6c4d85a7f1581c06dd3c6d4f3da5569676fc2e
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-17 12:23:23 +0800

    Add 正则表达式匹配

---
+++ 数据结构与算法/算法/正则表达式匹配/README.md
@@ -0,0 +1,3 @@
+# 正则表达式匹配
+
+[提交链接](https://www.nowcoder.com/practice/45327ae22b7b413ea21df13ee7d6429c?tpId=190&&tqId=36062&rp=1&ru=/ta/job-code-high-rd&qru=/ta/job-code-high-rd/question-ranking)
---
+++ 数据结构与算法/算法/正则表达式匹配/Solution.h
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool match(char* s, char* p) {
+        int lens = strlen(s);
+        int lenp = strlen(p);
+        if (lens == 0 && lenp == 0) return true;
+        if (lens != 0 && lenp == 0) return false;
+        if (*(p + 1) != '*') {
+            if (*s == *p || (*s != 0 && *p == '.'))
+                return match(s + 1, p + 1);
+            else 
+                return false;
+        } else {
+            if (*s == *p || (*s != 0 && *p == '.'))
+                return match(s, p + 2) || match(s + 1, p);
+            else 
+                return match(s, p + 2);
+        }
+    }
+};
