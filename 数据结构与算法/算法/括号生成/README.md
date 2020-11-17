44a9a6c3bb0dc4d9d78e39744a89215483058d90
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-17 10:19:16 +0800

    Add 括号生成

---
+++ 数据结构与算法/算法/括号生成/README.md
@@ -0,0 +1,3 @@
+# 括号生成
+
+[提交链接](https://www.nowcoder.com/practice/c9addb265cdf4cdd92c092c655d164ca?tpId=188&&tqId=36320&rp=1&ru=/ta/job-code-high-week&qru=/ta/job-code-high-week/question-ranking)
---
+++ 数据结构与算法/算法/括号生成/Solution.h
@@ -0,0 +1,29 @@
+class Solution {
+    void dfs(vector<string> &res, string str, int f, int left, int cur, int n) {
+        if (cur == n && !f) {
+            res.push_back(str);
+            return;
+        }
+        if (left) {
+            str += '(';
+            dfs(res, str, f + 1, left - 1, cur + 1, n);
+            str.pop_back();
+        }
+        if (f > 0) {
+            str += ')';
+            dfs(res, str, f - 1, left, cur + 1, n);
+            str.pop_back();
+        }
+    }
+public:
+    /**
+     * 
+     * @param n int整型 
+     * @return string字符串vector
+     */
+    vector<string> generateParenthesis(int n) {
+        vector <string> res;
+        dfs(res, "", 0, n, 0, n * 2);
+        return res;
+    }
+};
