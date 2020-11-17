5af3a087ef264f5f71f944efc2aa6769ed5975e3
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-17 12:29:23 +0800

    Add 判断一棵二叉树是否为搜索二叉树和完全二叉树

---
+++ 数据结构与算法/算法/判断一棵二叉树是否为搜索二叉树和完全二叉树/README.md
@@ -0,0 +1,3 @@
+# 判断一棵二叉树是否为搜索二叉树和完全二叉树
+
+[提交链接](https://www.nowcoder.com/practice/f31fc6d3caf24e7f8b4deb5cd9b5fa97?tpId=188&rp=1&ru=%2Fta%2Fjob-code-high-week&qru=%2Fta%2Fjob-code-high-week%2Fquestion-ranking)
---
+++ 数据结构与算法/算法/判断一棵二叉树是否为搜索二叉树和完全二叉树/Solution.h
@@ -0,0 +1,39 @@
+/**
+ * struct TreeNode {
+ *	int val;
+ *	struct TreeNode *left;
+ *	struct TreeNode *right;
+ * };
+ */
+
+class Solution {
+    int last = INT_MIN;
+    bool isSearch(TreeNode* rt) {
+        if (!rt) return true;
+        bool res = isSearch(rt->left);
+        if (res == false) return res;
+        if (last > rt->val) return false;
+        last = rt->val;
+        return isSearch(rt->right);
+    }
+    bool flag = false;
+    bool isComplete(TreeNode* rt) {
+        if (!rt) return true;
+        if (!rt->left && !rt->right) return true;
+        if (!rt->left && rt->right) return false;
+        if (rt->left && !rt->right) {
+            if (flag) return false;
+            flag = true;
+        }
+        return isComplete(rt->left) && isComplete(rt->right);
+    }
+public:
+    /**
+     * 
+     * @param root TreeNode类 the root
+     * @return bool布尔型vector
+     */
+    vector<bool> judgeIt(TreeNode* root) {
+        return { isSearch(root), isComplete(root) };
+    }
+};
