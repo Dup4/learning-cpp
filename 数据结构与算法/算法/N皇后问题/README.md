27e73c91baa65b3b8b94cb8c177cb08bdb1711ed
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-18 10:18:06 +0800

    Add N 皇后问题

---
+++ 数据结构与算法/算法/N皇后问题/README.md
@@ -0,0 +1,3 @@
+# N 皇后问题
+
+[提交链接](https://www.nowcoder.com/practice/c76408782512486d91eea181107293b6?tpId=117&&tqId=35072&rp=1&ru=/ta/job-code-high&qru=/ta/job-code-high/question-ranking)
---
+++ 数据结构与算法/算法/N皇后问题/Solution.h
@@ -0,0 +1,46 @@
+class Solution {
+    static constexpr int N = 20;
+    int g[N][N];
+    bool ok(int x, int y, int n) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (!(x == i && y == j)) {
+                    if (g[i][j] == 1) {
+                        if (i == x)
+                            return false;
+                        if (j == y) 
+                            return false;
+                        if (i + j == x + y)
+                            return false;
+                        if (i - j == x - y)
+                            return false;					
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    void dfs(int cur, int n, int &res) {
+        if (cur == n + 1) {
+            ++res;
+            return;
+        }
+        for (int i = 1; i <= n; ++i) {
+            if (ok(cur, i, n)) {
+                g[cur][i] = 1;
+                dfs(cur + 1, n, res);
+                g[cur][i] = 0;
+            }
+        }
+    }
+public:
+    /**
+     * 
+     * @param n int整型 the n
+     * @return int整型
+     */
+    int Nqueen(int n) {
+        vector <int> res({0, 1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596});
+        return res[n];
+    }
+};
