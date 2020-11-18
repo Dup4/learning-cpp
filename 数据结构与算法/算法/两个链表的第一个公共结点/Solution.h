4b50cfc6bf8b0f94daa1640e078070e6058c4539
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-11-18 11:58:32 +0800

    Add 两个链表的第一个公共结点

---
+++ 数据结构与算法/算法/两个链表的第一个公共结点/Solution.h
@@ -0,0 +1,12 @@
+class Solution {
+public:
+   ListNode* FindFirstCommonNode(ListNode* pHead1, ListNode* pHead2) {
+        ListNode *p1 = pHead1;
+        ListNode *p2 = pHead2;
+        while(p1 != p2) {
+            p1 = p1 ? p1->next : pHead2;
+            p2 = p2 ? p2->next : pHead1;
+        }
+        return p1;
+    }
+};
