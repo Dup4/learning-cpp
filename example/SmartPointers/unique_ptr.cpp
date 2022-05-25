97225dbe2a85ac70b197b7d86bc6ac073a5ec37c
Dup4 <lyuzhi.pan@gmail.com>
2022-05-25 09:20:03 +0800

    feat: unique_ptr

---
+++ example/SmartPointers/unique_ptr.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class Foo {
+public:
+    Foo() = delete;
+    Foo(int flag) : flag_(flag) {
+        cout << flag_ << " is constructed" << endl;
+    }
+
+    ~Foo() {
+        cout << flag_ << " is destructed" << endl;
+    }
+
+private:
+    int flag_;
+};
+
+void bar() {
+    auto a = std::make_unique<Foo>(1);
+
+    auto* b = new Foo(2);
+    auto b_ptr = std::unique_ptr<Foo>(b);
+
+    auto c = std::unique_ptr<Foo>(std::move(a));
+}
+
+int main() {
+    bar();
+
+    cout << "main endl" << endl;
+    return 0;
+}
