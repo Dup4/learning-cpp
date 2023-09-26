473c7cdbd40b24685b87e5fba5b86eb3fab32ca1
Dup4 <lyuzhi.pan@gmail.com>
2023-09-26 15:08:26 +0800

    feat: vector-bool
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- example/vector-bool/main.cc
+++ example/vector-bool/main.cc
@@ -3,8 +3,14 @@
 
 int main() {
     std::vector<bool> vec{true, false, true};
+    vec.push_back(false);
+    vec.reserve(100);
+    vec.resize(10);
+
     auto first = vec[0];
     first = false;
+
+    vec[0].flip();
 
     std::cout << vec[0] << std::endl;
     return 0;
